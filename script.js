8796bab6b2ef70b0d01c8364d27e1ffd15aeaddd
taitv1112 <91813628+taitv1112@users.noreply.github.com>
2021-10-26 22:47:32 +0700

    di chuyen don gian

--- script.js
+++ script.js
@@ -1,35 +1,62 @@
-/**
- * Created by nhatnk on 4/26/17.
- */
+class Hero {
+  image
+  top
+  left
+  size
+  speed
 
-function Hero(image, top, left, size){
-  this.image = image;
-  this.top = top;
-  this.left = left;
-  this.size = size;
-
-  this.getHeroElement = function(){
-    return '<img width="'+ this.size + '"' +
-      ' height="'+ this.size + '"' +
-      ' src="' + this.image +'"' +
-      ' style="top: '+this.top+'px; left:'+this.left+'px;position:absolute;" />';
+  constructor(image, top, left, size, speed) {
+    this.image = image;
+    this.top = top;
+    this.left = left;
+    this.size = size;
+    this.speed = speed;
   }
 
-  this.moveRight = function(){
-    this.left += 20;
+  getHeroElement() {
+    return '<img alt="khonghienthi" width="' + this.size + '"' +
+        ' height="' + this.size + '"' +
+        ' src="' + this.image + '"' +
+        ' style="top: ' + this.top + 'px; left:' + this.left + 'px;position:absolute;" />';
+  }
+
+  moveRight() {
+    this.left += this.speed;
     console.log('ok: ' + this.left);
   }
 
+  moveBottom() {
+    this.top += this.speed;
+    console.log('ok: ' + this.top);
+  }
+
+  moveLeft() {
+    this.left -= this.speed;
+    console.log('ok: ' + this.left);
+  }
+
+  moveTop() {
+    this.top -= this.speed;
+    console.log('ok: ' + this.top);
+  }
 }
 
-var hero = new Hero('pikachu.png', 20, 30, 200);
+let speed = +prompt("Nhập vào tốc độ: ")
+let hero = new Hero('naruto.jpg', 0, 0, 200, speed);
 
-function start(){
-  if(hero.left < window.innerWidth - hero.size){
+function start() {
+  if (hero.top === 0 && hero.left < window.innerWidth - hero.size) {
     hero.moveRight();
+  } else if (hero.top < window.innerHeight - hero.size && hero.left >= window.innerWidth - hero.size) {
+    hero.moveBottom();
+  } else if (hero.left > 0) {
+    hero.moveLeft()
+  } else if (hero.top > 0) {
+    hero.moveTop()
   }
   document.getElementById('game').innerHTML = hero.getHeroElement();
-  setTimeout(start, 500)
+  document.getElementById("play").style.display = "none"
+  setTimeout(start, 20)
 }
-
-start();
+//
+// start();
